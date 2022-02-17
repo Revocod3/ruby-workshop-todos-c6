84fba2c4cc573302e69729f3a5ac94dbb31b2c71
paulotijero <paulotijero@gmail.com>
2022-02-17 11:25:07 -0500

    CHORE: add block comments

--- todos.rb
+++ todos.rb
@@ -1,9 +1,12 @@
+# ====== Data - START
 todos = [
   { id: 1, content: "Fill the weekly feedback", completed: false },
   { id: 2, content: "Complete Ruby Basics 1",  completed: false  },
   { id: 3, content: "Complete Ruby Basics 2",  completed: false  }
 ]
+# ====== Data -END
 
+# ====== Methods - START
 def print_actions_menu
   puts "-" * 65
   puts "add | list | completed | toggle | delete | exit"
@@ -14,5 +17,8 @@ def todo_list(todos)
     puts "#{todo[:id]}. #{todo[:content]}"
   end
 end
+# ====== Methods - END
 
+# ====== Main - START
 todo_list(todos)
+# ====== Main - END
