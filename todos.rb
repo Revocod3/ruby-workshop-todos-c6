6aa5ca91c00d231bfadb410bd60a9d55635df066
Andore <andretj_92@live.com>
2022-02-17 13:21:42 -0500

    Create method to show the list of not completed todos

--- todos.rb
+++ todos.rb
@@ -1,8 +1,8 @@
 # ====== Data - START
 todos = [
   { id: 1, content: "Fill the weekly feedback", completed: false },
-  { id: 2, content: "Complete Ruby Basics 1",  completed: false  },
-  { id: 3, content: "Complete Ruby Basics 2",  completed: false  }
+  { id: 2, content: "Complete Ruby Basics 1", completed: false },
+  { id: 3, content: "Complete Ruby Basics 2", completed: false }
 ]
 # ====== Data -END
 
@@ -18,6 +18,11 @@ def todo_list(todos)
   todos.each do |todo|
     puts "#{todo[:id]}. #{todo[:content]}"
   end
+end
+
+def list_not_completed_todos(todos)
+  filteres_todos = todos.reject { |todo| todo[:completed] }
+  todo_list(filteres_todos)
 end
 # ====== Methods - END
 
