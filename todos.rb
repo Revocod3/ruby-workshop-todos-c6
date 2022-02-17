6587fedca8fb711150d295be0f4ab60a050fb266
paulotijero <paulotijero@gmail.com>
2022-02-17 10:34:11 -0500

    ADD: todo structure

--- todos.rb
+++ todos.rb
@@ -1,1 +1,7 @@
-# Happy coding 😎
+todos = [
+  { :id => 1, :content => "Fill the weekly feedback",  :completed => false  },
+  { :id => 2, :content => "Complete Ruby Basics 1",  :completed => false  },
+  { :id => 3, :content => "Complete Ruby Basics 2",  :completed => false  },
+]
+
+puts todos
