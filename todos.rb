c98b8e1d317450a5fce63db88f3d15dc604328b7
Andore <andretj_92@live.com>
2022-02-17 11:25:53 -0500

    Run rubocop to solve 12 offenses

--- todos.rb
+++ todos.rb
@@ -1,7 +1,7 @@
 todos = [
-  { :id => 1, :content => "Fill the weekly feedback",  :completed => false  },
-  { :id => 2, :content => "Complete Ruby Basics 1",  :completed => false  },
-  { :id => 3, :content => "Complete Ruby Basics 2",  :completed => false  },
+  { id: 1, content: "Fill the weekly feedback", completed: false },
+  { id: 2, content: "Complete Ruby Basics 1",  completed: false  },
+  { id: 3, content: "Complete Ruby Basics 2",  completed: false  }
 ]
 
 def print_actions_menu
